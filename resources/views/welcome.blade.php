bb8f65c66daebecb6d433ff2b37a0221e628f632
rahulalam31 <gaberialcramern31@outlook.com>
2024-05-23 21:38:00 +0530

    map blade update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,37 +3,57 @@
 
 <head>
     <title>Geolocation Info</title>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+
+    <style>
+        .container {
+            margin: auto;
+            width: 50%;
+            border: 3px solid green;
+            padding: 10px;
+        }
+
+        #map {
+            height: 300px;
+        }
+    </style>
 </head>
 
 <body>
-    <form method="GET" action="/">
-        <input type="text" name="ip" placeholder="Enter IP Address" required>
-        <button type="submit">Get Geolocation</button>
-    </form>
+    <div class="container">
+        <form method="POST" action="/">
+            @csrf
+            <input type="text" name="ip" placeholder="Enter IP Address" required>
+            <button type="submit">Get Geolocation</button>
+        </form>
 
-    @if (isset($error))
-        <p>{{ $error }}</p>
-    @else
-        <p>IP Address: {{ $ip }}</p>
-        <p>Country: {{ $country }}</p>
-        <p>Region: {{ $region }}</p>
-        <p>City: {{ $city }}</p>
-        <p>Latitude: {{ $latitude }}</p>
-        <p>Longitude: {{ $longitude }}</p>
-    <div id="map"></div>
+        @if (isset($geoData))
+            <p>IP Address: {{ $geoData->ip_address }}</p>
+            <p>Country: {{ $geoData->country }}</p>
+            <p>Region: {{ $geoData->region }}</p>
+            <p>City: {{ $geoData->city }}</p>
+            <p>Latitude: {{ $geoData->latitude }}</p>
+            <p>Longitude: {{ $geoData->longitude }}</p>
 
-    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-    <script>
-        var map = L.map('map').setView([{{ $latitude }}, {{ $longitude }}], 13);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '© OpenStreetMap contributors'
-        }).addTo(map);
-        L.marker([{{ $latitude }}, {{ $longitude }}]).addTo(map)
-            .bindPopup('Location: {{ $city }}, {{ $region }}, {{ $country }}')
-            .openPopup();
-    </script>
-    @endif
+            <div id="map"></div>
+            <script>
+                var map = L.map('map').setView([{{ $geoData->latitude }}, {{ $geoData->longitude }}], 13);
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '© OpenStreetMap contributors'
+                }).addTo(map);
+                L.marker([{{ $geoData->latitude }}, {{ $geoData->longitude }}]).addTo(map)
+                    .bindPopup('Location: {{ $geoData->city }}, {{ $geoData->region }}, {{ $geoData->country }}')
+                    .openPopup();
+            </script>
+        @else
+            <p>{{ $error }}</p>
+        @endif
+
+    </div>
 
 </body>
 
